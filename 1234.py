9e07347e944860fd527e03de96dd69f07f418596
Acer <ivanvenglesvkuy@gmail.com>
2024-04-25 15:57:10 +0300

    add fuleів

--- 1234.py
+++ 1234.py
@@ -15,21 +15,31 @@ class Pole:
 
     def print_footboler(self):
         if self.footboler != []:
-            print(f"Імена Футболістів{self.pol}:")
+            print(f"Склад команди{self.pol}:")
             for footboler in self.footboler:
                 print(footboler.name)
         else:
                 print(f"У {self.pol} немає пасажирів")
 
 
-fot1 = Human("Настя")
-fot2 = Human("Григорій")
-fot3 = Human("Вадим")
+fot1 = Human("==Ronaldo===Benzema===Bale==\n"
+             "============================\n"
+             "==Modrich============Kross==\n"
+             "============================\n"
+             "==========Casemiro==========\n"
+             "============================\n"
+             "==Marcelo=========Carvajal==\n"
+             "=======Ramos====Pepe========\n"
+             "============================\n"
+             "============Navas==========="
+             "")
+fot2 = Human("Матвій")
+fot3 = Human("Яник")
 
 pol1 = Pole(": на полі 1")
 
 
-pol1.add_footboler(fot1,fot2,fot3)
+pol1.add_footboler(fot1)
 
 
 pol1.print_footboler()
